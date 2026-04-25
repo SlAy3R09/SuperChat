2d0cfe5811ba3f0944cfd12d3bdefc7eced58b17
SlAy3R09 <ROBOBOGUS25@gmail.com>
2026-04-25 19:13:25 +0300

    55

---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+
+const indexHtmlFile = fs.readFileSync(path.join(__dirname, 'static', 'index.html'))
+const scriptFile = fs.readFileSync(path.join(__dirname, 'static', 'script.js'))
+const styleFile = fs.readFileSync(path.join(__dirname, 'static', 'style.css'))
+
+const server = http.createServer((req, res) => {
+switch (req.url) {
+   case '/': return res.end(indexHtmlFile);
+   case '/script.js': return res.end(scriptFile);
+   case '/style.css': return res.end(styleFile);
+    
+}
+res.statusCode = 404
+return res.end('Error 404')
+})
+
+server.listen(3000)
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected. id - ' + socket.id);
+  socket.on('new_message', (message) => {
+    io.emit('message', userNickname + ' : ' + message);
+  });
+})
+
---
+++ static/script.js
@@ -0,0 +1,16 @@
+const socket = io();
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('new_message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('message', function(msg) {
+    var item = document.createElement('li');
+    item.textContent = msg;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
+});
